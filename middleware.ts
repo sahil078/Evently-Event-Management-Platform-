d40dbeac1d4f4f7bb78ce28576060a8dddb72125
Md Sahil <mdsahil31818@gmail.com>
2024-09-16 19:56:47 +0530

    final changes

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,39 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+  // publicRoutes: [
+  //   '/',
+  //   '/events/:id',
+  //   '/api/webhook/clerk',
+  //   '/api/webhook/stripe',
+  //   '/api/uploadthing'
+  // ],
+  // // Define the ignored routes that should not be authenticated
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ],
+// });
 
-export default clerkMiddleware({
-  // Define the public routes that don't require authentication
-  publicRoutes: [
-    '/',
+// Define which routes Clerk middleware s
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+   '/sign-up(.*)',
+   '/',
     '/events/:id',
     '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing'
-  ],
-  // Define the ignored routes that should not be authenticated
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-});
+  ])
 
-// Define which routes Clerk middleware should apply to
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
